fd6654991df35d8538ea00cad4df5a3e54d5a72d
tairtahar <tair31@gmail.com>
2021-09-11 19:35:41 +0200

    LeNet wirks with upgraded tf but test acc higher than train

--- lenet2.py
+++ lenet2.py
@@ -1,6 +1,7 @@
 from tensorflow.keras import layers, models
 from tensorflow.keras.layers import Dense
 import tensorflow as tf
+import tensorflow.keras.backend as K
 
 
 class LeNet(tf.keras.Model):
@@ -38,9 +39,10 @@ class LeNet(tf.keras.Model):
     def train(self, x_train, y_train, x_val, y_val, batch_size=128, epochs=5, verbose=0):
         history = self.model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs,
                                  validation_data=(x_val, y_val),
-                                 verbose=verbose
+                                 verbose=verbose,
                                  )
         print("model training done")
+        return history
 
     def evaluate(self, x_test, y_test, verbose=0):
         score = self.model.evaluate(x_test, y_test, verbose=0)
@@ -52,7 +54,7 @@ class LeNetBN1(LeNet):
     def __init__(self, input_shape, output_size=10):
         super().__init__(input_shape)
         # self.norm1 = layers.Lambda(batch_norm)(self.c1)
-        self.affine1 = BatchNormLayer()(self.c1)
+        self.affine1 = BatchNormLayer(input_shape)(self.c1)
         self.s2 = layers.AveragePooling2D(padding='valid')(self.affine1)
         # self.bn2 = layers.Lambda(batch_norm)(self.c3)
         # self.s4 = layers.AveragePooling2D(padding='valid')(self.bn2)
@@ -60,11 +62,9 @@ class LeNetBN1(LeNet):
 
 
 class BatchNormLayer(tf.keras.layers.Layer):
-    def __init__(self):
+    def __init__(self, input_shape):
         super().__init__()
         # self.units = units
-
-    def build(self, input_shape):  # Create the state of the layer (weights)
         gamma_init = tf.random_normal_initializer()
         self.gamma = tf.Variable(
             initial_value=gamma_init(shape=(input_shape[1:]),
@@ -75,11 +75,22 @@ class BatchNormLayer(tf.keras.layers.Layer):
             initial_value=beta_init(shape=(input_shape[1:]), dtype='float32'),
             trainable=True)
 
+    # def build(self, input_shape):  # Create the state of the layer (weights)
+    # gamma_init = tf.random_normal_initializer()
+    # self.gamma = tf.Variable(
+    #     initial_value=gamma_init(shape=(input_shape[1:]),
+    #                              dtype='float32'),
+    #     trainable=True)
+    # beta_init = tf.zeros_initializer()
+    # self.beta = tf.Variable(
+    #     initial_value=beta_init(shape=(input_shape[1:]), dtype='float32'),
+    #     trainable=True)
+
     def call(self, inputs):  # , training=None):  # Defines the computation from inputs to outputs
         # if training:
         epsilon = 0.00000001
-        mu = k.mean(inputs, axis=0)
-        variance = k.var(inputs)
+        mu = K.mean(inputs, axis=0)
+        variance = K.var(inputs)
         x_hat = (inputs - mu) / (variance + epsilon)
         # gamma_rep = k.expand_dims(self.gamma, axis=1)
         # beta_rep = k.expand_dims(self.beta, axis=1)
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from lenet2 import LeNet
+from lenet2 import LeNet, LeNetBN1
 import tensorflow as tf
+import matplotlib.pyplot as plt
+
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-input_shape = x_train.shape[1:]
-print(x_train.shape)
 x_train = tf.pad(x_train, [[0, 0], [2, 2], [2, 2]]) / 255
 x_test = tf.pad(x_test, [[0, 0], [2, 2], [2, 2]]) / 255
+indices = tf.range(start=0, limit=tf.shape(x_train)[0], dtype=tf.int32)
+shuffled_indices = tf.random.shuffle(indices)
+x_train = tf.gather(x_train, shuffled_indices)
+y_train = tf.gather(y_train, shuffled_indices)
+
 x_train = tf.expand_dims(x_train, axis=3, name=None)
 x_test = tf.expand_dims(x_test, axis=3, name=None)
 x_val = x_train[-2000:, :, :, :]
@@ -14,6 +19,28 @@ x_train = x_train[:-2000, :, :, :]
 y_train = y_train[:-2000]
 input_shape = x_train.get_shape().as_list()[1:]
 lenet_model = LeNet(input_shape=input_shape, output_size=10)
-lenet_model.model_compilation(optimizer='adam')
-lenet_model.train(x_train, y_train, x_val, y_val, batch_size=256, epochs=5, verbose=1)
+# lenet_model = LeNetBN1(input_shape=input_shape, output_size=10)
+lenet_model.model_compilation(optimizer='sgd')
+history = lenet_model.train(x_train, y_train, x_val, y_val, batch_size=256, epochs=5, verbose=1)  # steps_per_epoch=200,
 lenet_model.evaluate(x_test, y_test, verbose=1)
+
+
+# Accuracy plot
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'val'], loc='upper left')
+# plt.savefig('accuracy.pdf')
+plt.show()
+# Loss plot
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'val'], loc='upper left')
+# plt.savefig('loss.pdf')
+plt.show()
+
