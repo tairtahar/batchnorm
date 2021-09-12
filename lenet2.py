50a249663965233f7732c66536e6cabe381771bc
tairtahar <tair31@gmail.com>
2021-09-12 19:40:14 +0200

    adding another batchnorm, sigma as activation and shuffle all data

--- lenet2.py
+++ lenet2.py
@@ -62,23 +62,30 @@ class LeNetBN1(LeNet):
         self.model = models.Model(inputs=self.input1, outputs=self.output_layer)
 
 
+class LeNetBN2(LeNet):
+    def __init__(self, input_shape, batch_size, output_size=10):
+        super().__init__(input_shape)
+        # self.norm1 = layers.Lambda(batch_norm)(self.c1)
+        self.affine1 = BatchNormLayer(self.c1.shape[1:], batch_size)(self.c1)
+        self.s2 = layers.AveragePooling2D(padding='valid')(self.affine1)
+        self.affine2 = BatchNormLayer(self.c3.shape[1:], batch_size)(self.c3)
+        self.s4 = layers.AveragePooling2D(padding='valid')(self.affine2)
+        self.model = models.Model(inputs=self.input1, outputs=self.output_layer)
+
+
 class BatchNormLayer(tf.keras.layers.Layer):
     def __init__(self, input_shape, batch_size):
         super().__init__()
         # self.units = units
         gamma_init = tf.ones_initializer()
         self.gamma = tf.Variable(
-            initial_value=gamma_init(shape=input_shape,
-                                     dtype='float32'),
-            trainable=True)
+            initial_value=gamma_init(shape=input_shape, dtype='float32'), trainable=True)
         beta_init = tf.zeros_initializer()
         self.beta = tf.Variable(
-            initial_value=beta_init(shape=input_shape, dtype='float32'),
-            trainable=True)
+            initial_value=beta_init(shape=input_shape, dtype='float32'), trainable=True)
         self.batch_size = batch_size
 
-    def call(self, inputs):  # , training=None):  # Defines the computation from inputs to outputs
-        # if training:
+    def call(self, inputs, training=None):  # Defines the computation from inputs to outputs
         epsilon = 0.00000001
         mu = K.mean(inputs, axis=(0, 1, 2), keepdims=True)
         variance = K.var(inputs, axis=(0, 1, 2), keepdims=True)
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from lenet2 import LeNet, LeNetBN1
+from lenet2 import LeNet, LeNetBN1, LeNetBN2
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+# shuffling the data, making sure the division is not biased
+
+x = tf.concat([x_train, x_test], 0)
+y = tf.concat([y_train, y_test], 0)
+indices = tf.range(start=0, limit=tf.shape(x)[0], dtype=tf.int32)
+tf.random.set_seed(123)
+shuffled_indices = tf.random.shuffle(indices)
+x = tf.gather(x, shuffled_indices)
+y = tf.gather(y, shuffled_indices)
+x_train = x[:-10000, :, :]
+x_test = x[-10000:, :, :]
+y_train = y[:-10000]
+y_test = y[-10000:]
+
+###############
 x_train = tf.pad(x_train, [[0, 0], [2, 2], [2, 2]]) / 255
 x_test = tf.pad(x_test, [[0, 0], [2, 2], [2, 2]]) / 255
 indices = tf.range(start=0, limit=tf.shape(x_train)[0], dtype=tf.int32)
@@ -20,11 +35,11 @@ y_train = y_train[:-2000]
 input_shape = x_train.shape[1:]
 # lenet_model = LeNet(input_shape=input_shape, output_size=10)
 batch_size = 256
-lenet_model = LeNetBN1(input_shape=input_shape, batch_size=batch_size, output_size=10)
+lenet_model = LeNetBN2(input_shape=input_shape, batch_size=batch_size, output_size=10)
 lenet_model.model_compilation(optimizer='adam')
-history = lenet_model.train(x_train, y_train, x_val, y_val, batch_size=batch_size, epochs=15, verbose=1)  # steps_per_epoch=200,
+history = lenet_model.train(x_train, y_train, x_val, y_val, batch_size=batch_size, epochs=15,
+                            verbose=1)  # steps_per_epoch=200,
 lenet_model.evaluate(x_test, y_test, verbose=1)
-
 
 # Accuracy plot
 plt.plot(history.history['accuracy'])
@@ -44,4 +59,3 @@ plt.xlabel('epoch')
 plt.legend(['train', 'val'], loc='upper left')
 # plt.savefig('loss.pdf')
 plt.show()
-
