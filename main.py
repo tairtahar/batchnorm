3f33b79abc4a0f63dc5b7cc91d91c0ed156c9278
tairtahar <tair31@gmail.com>
2021-09-20 11:45:12 +0200

    visualization in the same axes for better comparison

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
 
     # Parameters definition. Please adjust params and choose a network
     batch_size = 64
-    epochs = 25
+    epochs = 10
     output_size = 10  # mnist has 10 possible classes
     optimizer = 'adam'
     window_size = 5  # for the moving average in the last part of batchnorm algorithm
@@ -18,20 +18,24 @@ def main():
     '''You cab choose (copy to the next line) one of the following:
     (1)'lenet' (no BN) ; (2)'lenet_bn1' (first conv layer has BN) ; (3)'lenet_bn2' (first+second conv layers have BN);
     (4)lenet_fc_bn1 (conv+first FC layer have BN) ; (5)'lenet_fc_bn2' (all layers with BN);'''
-    network = 'lenet'
-    # Model creation and training
-    history = models_handling.model_execution(network=network,
-                                              data=data,
-                                              output_size=output_size,
-                                              batch_size=batch_size,
-                                              optimizer=optimizer,
-                                              epochs=epochs,
-                                              window_size=window_size,
-                                              verbose=verbose)
-    # Visualizations of the training process
+    networks = ['lenet', 'lenet_bn1' , 'lenet_bn2', 'lenet_fc_bn1', 'lenet_fc_bn2']
+    histories = []
+    for i in range(len(networks)):
+        network = networks[i]
+        # Model creation and training
+        history = models_handling.model_execution(network=network,
+                                                  data=data,
+                                                  output_size=output_size,
+                                                  batch_size=batch_size,
+                                                  optimizer=optimizer,
+                                                  epochs=epochs,
+                                                  window_size=window_size,
+                                                  verbose=verbose)
+        histories.append(history)
+        # Visualizations of the training process
     if flag_visualizations:
-        visualizations.plot_accuracy(history)
-        visualizations.plot_loss(history)
+        visualizations.plot_accuracies(histories, networks)
+        # visualizations.plot_loss(history)
 
 
 if __name__ == '__main__':
--- visualizations.py
+++ visualizations.py
@@ -26,3 +26,17 @@ def plot_loss(history):
     plt.legend(['train', 'val'], loc='upper left')
     # plt.savefig('loss.pdf')
     plt.show()
+
+
+def plot_accuracies(histories, networks):
+    for i in range(len(histories)):
+        plt.plot(histories[i].history['accuracy'])
+    plt.yticks(np.arange(0.5, 1, 0.02))
+    plt.grid(which='major', linestyle=':', alpha=0.6)
+    plt.ylim([0.7, 1])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(networks, loc='lower right')
+    # plt.savefig('accuracy.pdf')
+    plt.show()
